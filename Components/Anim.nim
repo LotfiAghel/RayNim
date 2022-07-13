4d774e062b0275b0ea011c72923e49c7aea87d90
f <fatholah@gmail.com>
2022-07-14 03:46:40 +0430

    change electric animation

--- Components/Anim.nim
+++ Components/Anim.nim
@@ -79,9 +79,10 @@ type
   MeshElectirc* = ref object of AnimComp
     time*: ValueProvider[float]
     path*: seq[PathPoint]
-    path2*: seq[PathPoint]
     path0*: seq[PathPoint]
-    rands:seq[float]
+    position:seq[float]
+    vel:seq[float]
+    left*,right*:float
     mesh*: ptr Mesh
     radiusPoints*: seq[float]
     txtRadiusPoints*: seq[float]
@@ -291,17 +292,40 @@ proc getRandomSeq*(rands:var seq[float],n:int,l,r:float,rate:int)=
     if(rand(rate)==1):
       rands[i]= rand(r)
 
+proc randRange(l,r:float):float=
+  return l+rand(r-l)
+
+proc getRandomSeq2*(rands:var seq[float],pos:var seq[float],n:int,rate:int,l,r:float)=
+  rands.setLen n;
+  for i in 0..<n :
+    if pos[i]<l :
+        pos[i] = l
+        rands[i] = 10.0
+    if(rand(rate)==0):
+      #pos[i]-l --  r-pos[i]
+      var t=randRange(l,r)
+      rands[i]= (t-pos[i])*0.3
+      
+      #[if t<pos[i]:
+        rands[i]= -randRange(t,pos[i])
+      else:
+        rands[i]= randRange(pos[i],t)]#
+
+
 method update*(a: MeshElectirc) =
   a.time.update()
   var t = a.time.value
   
   var n=a.path.len;
   a.path0.setLen(n)
+  a.position.setLen(n)
   #if(t<0.1):
-  a.rands.getRandomSeq(n,-20,20,3)
+  a.vel.getRandomSeq2(a.position,n,1,a.left,a.right)
   for i in 0..<n:
-    a.path0[i].pos = a.path[i].pos + a.path2[i].normal*a.rands[i]#*sin(t) #*(1-t);
+    a.position[i] += a.vel[i]
+    a.path0[i].pos = a.path[i].pos + a.path0[i].normal*a.position[i]#*sin(t) #*(1-t);
     a.path0[i].normal = a.path[i].normal
+    
   #for i in 0..<n:
   #  a.path[i].pos = a.path[i].pos + a.path[i].normal*((rand(2.0)-1.0)*10);
 
